ac351b743780ba50313a8da9cf18e3a13c1d161f
MaxGroshev <groshevmaxim84@yandex.ru>
2025-11-28 22:35:50 +0300

    change git root
    Signed-off-by: MaxGroshev <groshevmaxim84@yandex.ru>

--- src/main/cpp/lzw/debug_utils/logs/logs_define.h
+++
@@ -1,9 +0,0 @@
-#if defined(MY_DEBUG)
-#define LOG_DEBUG(args...) \
-        write_logs (args); \
-
-#else
-#define LOG_DEBUG(args...);
-#endif
-
-//-----------------------------------------------------------------------------------------
--- src/main/cpp/lzw/tests/end_to_end_tests/clean_output.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-rm -r $(dirname $0)/my_decompress_dat/*.decode
-rm -r $(dirname $0)/my_compress_dat/*.lzw
-rm -r $(dirname $0)/my_compress_dat/*.zstd
-rm -r $(dirname $0)/my_compress_dat/*.zst
-
